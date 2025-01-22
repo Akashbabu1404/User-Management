ce8cb21d39fa91d44c8058afa0565b20b74c3d54
Akashbabu1404 <akashmbabu@gmail.com>
2025-01-22 12:34:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; // Updated import for React 18+
+import App from "./App";
+import "./index.css";
+
+// Create a root element and render the App
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
